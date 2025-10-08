581caca0a02564bd7c9cfec3056165418a6beabc
Julien Oyer <oyer.ludovic@gmail.com>
2025-10-08 16:31:12 +0200

    Ajout de la génération et du commit de Q4_archive_JJ-MM-AAAA.txt dans le workflow Q4 Correction de l'affichage dans Q3.java pour utiliser System.out au lieu de System.err Ajout de la vérification et du commit de Q2_archive_TOTO.txt dans le workflow Q2

--- Q4.java
+++ Q4.java
@@ -1,9 +1,13 @@
-public class Q3 {
+public class Q4 {
 	public static void main(String[] a){
 		int n = Integer.parseInt(a[0]);
 
 		for(int i = 0; i < n; i++){
-			System.out.print(i+" ");
+
+			for(int j = 0; j < n; j++){
+				System.out.print(j+" ");
+			}
+			System.out.println();
 		}
 	}
 }
