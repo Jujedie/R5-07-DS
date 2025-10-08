93661652984c6a4878c5d78aa2f9b918458ebb50
Julien Oyer <oyer.ludovic@gmail.com>
2025-10-08 16:45:01 +0200

    Ajout du workflow Q5 pour générer et pousser Q5_archive_JJ-MM-AAAA.txt et création de la classe Q5.java

---
+++ Q5.java
@@ -0,0 +1,12 @@
+public class Q5 {
+	public static void main(String[] a){
+		int n = Integer.parseInt(a[0]);
+
+		for(int i = 0; i < n; i++){
+			for(int j = n-1; j >= 0; j--)
+				System.out.print(j+" ");
+			
+			System.out.println();
+		}
+	}
+}
